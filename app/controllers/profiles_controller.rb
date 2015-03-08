4bd50609e791f53f801d5fd8672d7e0da8c42d3f
Sade Stevens <sadestevens.hc@gmail.com>
2015-03-08 09:18:10 -0400

    fixed profiles controller

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,17 +1,17 @@
 class ProfilesController < ApplicationController
 
-def index
-  @profiles = Profile.all
-end
+  def index
+    @profiles = Profile.all
+  end
 
-def new
-  @user = User.find params[:id]
-  @profile = Profile.new
-end
+  def new
+    @user = User.find params[:id]
+    @profile = Profile.new
+  end
 
-def show
-  @profile = Profile.find(params[:id])
-end
-
+  def show
+    @user = User.find params[:id]
+    @profile = Profile.all
+  end
 
 end
