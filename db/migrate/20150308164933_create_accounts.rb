3a29ddc209852ec1b4444c500494322efa8242f4
Sade Stevens <sadestevens.hc@gmail.com>
2015-03-08 12:52:23 -0400

    updated accounts migration

--- db/migrate/20150308164933_create_accounts.rb
+++ db/migrate/20150308164933_create_accounts.rb
@@ -1,7 +1,10 @@
 class CreateAccounts < ActiveRecord::Migration
   def change
     create_table :accounts do |t|
-
+      t.string     :f_name
+      t.string     :l_name
+      t.date       :born_on
+      t.datetime   :phone
     end
   end
 end
